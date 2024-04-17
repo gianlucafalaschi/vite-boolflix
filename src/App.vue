740a42770057f2d74137d7177ba51d8fb87ab612
gianlucafalaschi <gian100gian@outlook.it>
2024-04-17 15:34:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from 'axios';
+import { store } from './store.js';
+import AppFilter from './components/AppFilter.vue';
+
+export default {
+    components: {
+      AppFilter,
+    },
+    data() {
+      return {
+        store,
+      };
+    },
+    methods: {
+    
+    },
+    mounted () {
+
+    }
+  }
+
+</script>
+
+<template>
+  <AppFilter></AppFilter>
+</template>
+
+<style lang="scss">
+@use'./style/generic';
+</style>
---
+++ src/components/AppFilter.vue
@@ -0,0 +1,43 @@
+<script>
+import { store } from '../store.js';
+    
+export default {
+    name: 'AppFilter',
+    data(){
+        return {
+            store,
+            
+        };
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="logo-container">
+            <img src="" alt="logo">
+        </div>
+        <div class="searchbar-container">
+            <input type="text" placeholder="Search movie">
+        </div>
+        <div class="button-container">
+            <button>Search</button>
+        </div>
+    </div>
+    
+</template>
+
+<style scoped lang="scss">
+@use'../style/partials/_variables' as *;
+
+.container {
+    display: flex;
+    gap: 10px;
+    background-color: pink;
+    padding-top:10px;
+    padding-bottom:10px;
+}
+
+
+
+</style>
